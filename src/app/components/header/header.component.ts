6d936a76f2eaa1cc77a18c80021958d7c9c49087
TranBaoTran <117694181+TranBaoTran@users.noreply.github.com>
2024-10-29 14:03:24 +0700

    add header and footer

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,14 +2,27 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MdbDropdownModule } from 'mdb-angular-ui-kit/dropdown';
 import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, MdbDropdownModule, MdbCollapseModule],
+  imports: [CommonModule, MdbDropdownModule, MdbCollapseModule, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  checkboxes = Array(10).fill(0);
+  checkboxStates = Array(this.checkboxes.length).fill(false);
+  dropDownProvince = 'TP.HCM';
+  
+  updateDropdown(text: string, event: Event) {
+    event.preventDefault();
+    this.dropDownProvince = text;
+  }
+
+  uncheckAll() {
+    this.checkboxStates = this.checkboxStates.map(() => false);
+  }
 }
